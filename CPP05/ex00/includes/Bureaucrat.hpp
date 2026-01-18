c4267c53eaf248fc22d8a636a5f8eecdb9aab2f2
Bruno Thorstensen <129321906+brunothors@users.noreply.github.com>
2026-01-18 01:54:58 +0000

    CPP05

---
+++ CPP05/ex00/includes/Bureaucrat.hpp
@@ -0,0 +1,41 @@
+#ifndef BEREAUCRAT_HPP
+#define BEREAUCRAT_HPP
+
+#include <string>
+#include <exception>
+#include <iostream>
+#include <ostream>
+
+class Bureaucrat {
+	private:
+		const std::string	_name;
+		int					_grade;
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string &name, int const &grade);
+		Bureaucrat(const Bureaucrat &param);
+		~Bureaucrat();
+
+		Bureaucrat &operator=(const Bureaucrat &param);
+
+		const std::string	&getName(void) const;
+		const int			&getGrade(void) const;
+		void				setGrade(int const &grade);
+
+		void incrementGrade();
+		void decrementGrade();
+
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+};
+
+std::ostream &operator<<(std::ostream &oS, Bureaucrat const &param);
+
+#endif
---
+++ CPP05/ex00/srcs/main.cpp
@@ -0,0 +1,66 @@
+#include "Bureaucrat.hpp"
+
+int main() {
+  std::cout << std::endl;
+  std::cout << "Test increment a high grade." << std::endl;
+  {
+    try {
+      Bureaucrat john("High Grade Bure", 1);
+      std::cout << john << std::endl;
+
+      john.incrementGrade();
+      std::cout << john << std::endl;
+
+      john.decrementGrade();
+      std::cout << john << std::endl;
+
+      // This should throw GradeTooHighException
+    } catch (std::exception &e) {
+      std::cerr << "Exception: " << e.what() << std::endl;
+    }
+  }
+  std::cout << std::endl;
+  std::cout << "Test decrement a low grade." << std::endl;
+  {
+    try {
+      Bureaucrat john("Low Grade Bure", 150);
+      std::cout << john << std::endl;
+
+      john.decrementGrade();
+      std::cout << john << std::endl;
+
+      john.incrementGrade();
+      std::cout << john << std::endl;
+
+      // This should throw GradeTooHighException
+    } catch (std::exception &e) {
+      std::cerr << "Exception: " << e.what() << std::endl;
+    }
+  }
+  std::cout << std::endl;
+  std::cout << "Now different grade" << std::endl;
+  {
+    try {
+      Bureaucrat john("Mid Grade", 75);
+      std::cout << john << std::endl;
+
+      john.incrementGrade();
+      std::cout << john << std::endl;
+
+      john.decrementGrade();
+      std::cout << john << std::endl;
+
+      while (john.getGrade() > 42)
+        john.incrementGrade();
+      std::cout << john << std::endl;
+
+	  john.setGrade(151);
+
+      // This should throw GradeTooHighException
+    } catch (std::exception &e) {
+      std::cerr << "Exception: " << e.what() << std::endl;
+    }
+  }
+
+  return 0;
+}
